8cd885b0ff164cf334fc6f1929750415060fd3bf
Udbhas Dutta <udbhasdutta@gmail.com>
2024-07-09 12:29:10 +0530

    added deployment codes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -23,6 +24,7 @@ cloudinary.config({
 
 const app = express()
 const port = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 app.use(cors({
     origin: 'http://localhost:3000',
@@ -39,6 +41,14 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/notifications", notificationRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
